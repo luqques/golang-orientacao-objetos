300028b660d173889c488ae5f2381b62b78a628b
Lucas da Cunha <luques.cunha@gmail.com>
2026-03-03 15:35:04 -0300

    criando métodos da conta

--- contas/conta.go
+++ contas/conta.go
@@ -20,3 +20,18 @@ func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *Con
 	fmt.Printf("Conta origem %v está com o saldo total de %v.\n", c.NumeroConta, c.Saldo)
 	return true
 }
+
+func (c *ContaCorrente) Depositar(valorDeposito float64) {
+	c.Saldo += valorDeposito
+	fmt.Printf("Conta %v recebeu um depósito de %v reais, totalizando %v de saldo.\n", c.NumeroConta, valorDeposito, c.Saldo)
+}
+
+func (c *ContaCorrente) Sacar(valorSaque float64) bool {
+	if valorSaque > c.Saldo {
+		fmt.Printf("Saldo insuficiente para saque de %v reais. Saldo atual: %v reais.\n", valorSaque, c.Saldo)
+		return false
+	}
+	c.Saldo -= valorSaque
+	fmt.Printf("Conta %v realizou um saque de %v reais, saldo restante: %v reais.\n", c.NumeroConta, valorSaque, c.Saldo)
+	return true
+}
